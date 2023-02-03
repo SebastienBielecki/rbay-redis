8bd4d7accfb09847066d03d9fa13cd6c5d816193
Sebastien <seb_bielecki@hotmail.com>
2023-02-03 17:52:29 -0600

    creating amd fetching datas with Redis
    - create and fetch user
    - create and fetch session
    - create and fetch item
    - helpers: serialize and deserialize
    - helpers: generate Redis keys for users, sessions and items

--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,34 @@
 import type { Session } from '$services/types';
+import { client } from '../redis/client'
+import { sessionsKey } from '../keys';
 
-export const getSession = async (id: string) => {};
 
-export const saveSession = async (session: Session) => {};
+export const getSession = async (id: string) => {
+    const session = await client.hGetAll(sessionsKey(id))
+    console.log(session);
+    if (Object.keys(session).length === 0) {
+        return null
+    }
+    return deserialize(id, session)
+};
+
+
+
+export const saveSession = async (session: Session) => {
+    return client.hSet(sessionsKey(session.id), serialize(session))
+};
+
+const deserialize = (id: string, session: {[key: string]: string}) => {
+    return {
+        id,
+        userId: session.userId,
+        username: session.username
+    }
+}
+
+const serialize = (session: Session) => {
+    return {
+        userId: session.userId,
+        username: session.username
+    }
+}
