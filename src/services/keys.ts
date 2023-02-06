727b91d83ea2ead49bb763ccf5f485be3baa1607
Sebastien <seb_bielecki@hotmail.com>
2023-02-06 14:02:31 -0600

    FEAT: show bid history
    - implemented with a Redis list data structure

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -15,3 +15,5 @@ export const itemsKey = (itemId: string) => `ìtems#${itemId}`
 export const itemsByViewsKey = () => 'items:views'
 export const itemsByEndingAtKey = () => 'items:endingAt'
 export const itemsViewsKey = (itemId: string) => `items:views#${itemId}`
+export const bidHistoryKey = (itemId: string) => `history#${itemId}`
+
